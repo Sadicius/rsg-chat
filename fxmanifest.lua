8f3dd1b0e057a7d0137d6ce87b1e4660cb228ba7
Rehanniz <67835586+Rehanniz@users.noreply.github.com>
2024-10-27 12:52:01 +0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-chat'
+
+ui_page 'web/ui.html'
+
+files {
+    'web/*.*',
+}
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client/client.lua',
+    'client/whisper.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+    'server/commands.lua',
+}
+
+dependency 'rsg-core'
