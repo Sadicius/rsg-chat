9e06179075a5a3347868028094414d4f9eab307c
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-10-28 19:19:45 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -76,10 +76,10 @@ Config.mpCommand = 'mp'
 -- EXTRA Webhooks / RANKING
 -----------------------
 Config.Webhooks = {
-    ["chatreport"] = "https://discord.com/api/webhooks/1260635717483233310/JsHUhvjyWoJOLgTnPATvM-Gl7p7kYZnbdsF48VNjZ2i6OuS81RxqIDgZ1G2_GUq3QtBN",
-    ["chatooc"] = "https://discord.com/api/webhooks/1260635625137115227/8cwmcP9DQEllBaTix2ulkz8Z_XOnp1af0JZMPyw1cH2IQZ4PRoekWmgn9520R7Tn-S-m",
-    ["chatme"] = "https://discord.com/api/webhooks/1260635797539786834/r0QnbzAeFLRhVtW9Xy4F3DxRYbQTeznIykgA2qD7NJS5cV_rCKHdZLSek6xvF7jJrjsR",
-    ["chatdo"] = "https://discord.com/api/webhooks/1260635881979510945/QmA_nM075Na8CAw-_z5bChmc65wXtNK9ihe_Jganz5iWeqCwSo8PrzFfHhY3UADf6p2f",
-    ["chatrumor"] = "https://discord.com/api/webhooks/1260635955874762803/F9SI-BK0FpVg29WTnR7wTzjXMb5W9oZGzaWPur-m8sWGfC9hc8HhNWiLgLv70tx7b-jo",
-    ["chatrumoric"] = "https://discord.com/api/webhooks/1257638072858968105/Wzog2bgrEApeLvXnAenhGRE9a26x80V5OuAL-aamlNnuCbuAvBqlZ1b-KgrZuV5esJ9W",
+    ["chatreport"] = "",
+    ["chatooc"] = "",
+    ["chatme"] = "",
+    ["chatdo"] = "",
+    ["chatrumor"] = "",
+    ["chatrumoric"] = "",
 }
