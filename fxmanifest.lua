20ccb925751603352e4d52c86028f966b65d8376
Rehan Nizamani <67835586+Rehanniz@users.noreply.github.com>
2024-11-09 15:50:51 +0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
+
 description 'rsg-chat'
+version "1.0"
 
 ui_page 'web/ui.html'
 
